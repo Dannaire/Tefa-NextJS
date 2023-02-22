a5af051f0653556cb7a00249389e124f6072665c
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2023-02-22 21:32:04 +0700

    font

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* link your fonts here */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css?family=Syne"
+            rel="stylesheet"
+          />
+        </Head>
       <body>
         <Main />
         <NextScript />
