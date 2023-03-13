09df4b18dca3bb07edfd2ff6280008010e950109
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2023-03-13 15:21:40 +0700

    add fetch api

--- components/card.jsx
+++ components/card.jsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
-import products from '../data/products.json';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -9,50 +8,64 @@ export default function Card() {
   const [filter, setFilter] = useState('');
   const [searchValue, setSearchValue] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
 
   const handleBuyNowClick = (productId) => {
     router.push(`/detail/${productId}`);
   };
 
-//   const handleFilter = (event) => {
-//     setFilter(event.target.value);
-//   };
+  const handleFilter = (event) => {
+    setFilter(event.target.value);
+  };
 
   const handleSearch = (event) => {
     setSearchValue(event.target.value);
   };
 
   useEffect(() => {
-    const filteredResults = products.filter((product) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.jsonbin.io/v3/b/640ed609c0e7653a0586fb32/latest');
+        const data = await response.json();
+        setSearchResults(data.record);
+        console.log('API data fetched successfully!');
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const filteredResults = searchResults.filter((product) => {
       return product.category.includes(filter) && product.name.toLowerCase().includes(searchValue.toLowerCase());
     });
-    setSearchResults(filteredResults);
-  }, [filter, searchValue]);
+    setFilteredResults(filteredResults);
+  }, [filter, searchValue, searchResults]);
 
   return (
     <>
       <div className='container'>
-     
         <h1 className='title'>Best Seller</h1>
         <p className='subtitle'>Check out our most popular products</p>
         <div className='filter-bar'>
           <input type='text' placeholder='Search products...' value={searchValue} onChange={handleSearch} />
-          {/* <select onChange={handleFilter}>
+          <select onChange={handleFilter}>
             <option value=''>All categories</option>
-            <option value='converse'>converse</option>
+            <option value='converse'>Converse</option>
             <option value='clothing'>Clothing</option>
             <option value='accessories'>Accessories</option>
-          </select> */}
+          </select>
         </div>
         <div className='card-group'>
-          {searchResults.map((product, index) => (
+          {filteredResults.map((product, index) => (
             <div className='card-home' key={index}>
               <Image src={product.image} alt={product.name} width={500} height={500} />
               <div className='details'>
                 <h3>{product.name}</h3>
                 <p className='price'>$ {product.price}</p>
                 <button onClick={() => handleBuyNowClick(product.id)} className="buy-button">Buy Now</button>
-
               </div>
             </div>
           ))}
